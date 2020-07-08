3e5d5d79617483799bc6e9fd223b6f3951ec6dfe
Raghav khanna <48377506+raghavkhanna30@users.noreply.github.com>
2020-07-08 17:25:48 +0530

    Create mainbot.py

---
+++ mainbot.py
@@ -0,0 +1,51 @@
+import tweepy
+import logging
+from config import create_api
+import time
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def validate_follower_count(user):
+    # update string split if you don't use this naming format for twitter profile:
+    # 'insert_your_name|{emoji_follower_count(user)} Followers'
+    current_follower_count = user.name.replace('|', ' ').split()
+    return current_follower_count[2]
+
+
+def emoji_follower_count(user):
+    emoji_numbers = {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
+                     4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣", 8: "8️⃣", 9: "9️⃣"}
+
+    follower_count_list = [int(i) for i in str(user.followers_count)]
+
+    emoji_followers = ''.join([emoji_numbers[k]
+                               for k in follower_count_list if k in emoji_numbers.keys()])
+
+    return emoji_followers
+
+
+def main():
+    api = create_api()
+
+    while True:
+        # change to your own twitter_handle
+        user = api.get_user('your_username')
+
+        if validate_follower_count(user) == emoji_follower_count(user):
+            logger.info(
+                f'You still have the same amount of followers, no update neccesary: {validate_follower_count(user)} -> {emoji_follower_count(user)}')
+        else:
+            logger.info(
+                f'Your amount of followers has changed, updating twitter profile: {validate_follower_count(user)} -> {emoji_follower_count(user)}')
+            # Updating your twitterprofile with your name including the amount of followers in emoji style
+            api.update_profile(
+                name=f'insert_your_name|{emoji_follower_count(user)} Followers')
+
+        logger.info("Waiting to refresh..")
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    main()
